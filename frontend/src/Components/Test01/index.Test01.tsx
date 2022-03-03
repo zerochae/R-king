e07925df34b9710585c85bf4c97ec8f620d855bb
zerochae <zerochae@kakao.com>
2022-03-03 14:54:27 +0900

    Feat: TypeScript 초기 설정

---
+++ frontend/src/Components/Test01/index.Test01.tsx
@@ -0,0 +1,11 @@
+import * as S from "Components/Test01/style.Test01";
+
+const Test01 = () => {
+  return (
+    <>
+      <S.Container></S.Container>
+    </>
+  );
+};
+
+export default Test01;
---
+++ frontend/src/Components/Test02/index.Test02.tsx
@@ -0,0 +1,11 @@
+import * as S from "Components/Test02/style.Test02";
+
+const Test02 = () => {
+  return (
+    <>
+      <S.Container></S.Container>
+    </>
+  );
+};
+
+export default Test02;
